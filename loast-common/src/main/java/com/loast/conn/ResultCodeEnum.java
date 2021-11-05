0f22306f79ffd96004f9f165b343f1b84f1cf303
Arman <life.is2short@outlook.com>
2021-11-05 15:34:23 +0800

    如果没有激活账号就拒绝登陆

--- loast-common/src/main/java/com/loast/conn/ResultCodeEnum.java
+++ loast-common/src/main/java/com/loast/conn/ResultCodeEnum.java
@@ -19,19 +19,14 @@ public enum ResultCodeEnum {
 
     FILE_UPLOAD_ERROR(false, 21004, "文件上传错误"),
     FILE_DELETE_ERROR(false, 21005, "文件刪除错误"),
-    EXCEL_DATA_IMPORT_ERROR(false, 21006, "Excel 数据导入错误"),
 
     FETCH_ACCESSTOKEN_FAILD(false, 23003, "获取 accessToken 失败"),
     FETCH_USERINFO_ERROR(false, 23004, "获取用户信息失败"),
     REGISTER_ERROR(false, 23006, "注册失败"),
 
-    COMMENT_EMPTY(false, 24006, "评论内容必须填写"),
-
     CODE_ERROR(false, 28000, "验证码错误"),
 
     LOGIN_PHONE_ERROR(false, 28009, "手机号码不正确"),
-    LOGIN_MOBILE_ERROR(false, 28001, "账号不正确"),
-    LOGIN_PASSWORD_ERROR(false, 28008, "密码不正确"),
 
 
     LOGIN_DISABLED_ERROR(false, 28002, "该用户已被禁用"),
@@ -40,7 +35,7 @@ public enum ResultCodeEnum {
     LOGIN_AUTH(false, 28004, "需要登录"),
     LOGIN_ACL(false, 28005, "没有权限"),
     EMAIL_SEND_ERROR(false, 28006, "邮件发送失败"),
-    SMS_SEND_ERROR_BUSINESS_LIMIT_CONTROL(false, 28007, "短信发送过于频繁"),
+    USER_INACTIVE(false, 28008, "请激活后账号后登陆"),
 
     LOGIN_FAIL(false, 30000, "用户名或密码错误"),
     USER_NOT_EXIST(false, 30001, "用户不存在"),
