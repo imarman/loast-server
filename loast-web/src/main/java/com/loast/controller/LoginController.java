72165ad98388a597e7d661824e09f3217fbf86b1
Arman <life.is2short@outlook.com>
2021-11-05 17:15:55 +0800

    fix:一些小问题

--- loast-web/src/main/java/com/loast/controller/LoginController.java
+++ loast-web/src/main/java/com/loast/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.loast.model.UserModel;
 import com.loast.result.R;
 import com.loast.service.ILoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
+ * 登陆&登出接口
+ *
  * @author Arman
  * @date 2021/11/5 11:38
  */
@@ -24,6 +27,11 @@ public class LoginController {
     @Resource
     ILoginService loginService;
 
+    /**
+     * 登陆
+     * @param userModel 用户对象
+     * @return token 的信息
+     */
     @PostMapping("/login")
     public R login(@RequestBody UserModel userModel) {
         if (StpUtil.isLogin()) {
@@ -34,6 +42,11 @@ public class LoginController {
         return R.ok(tokenInfo);
     }
 
+    /**
+     * 退出登陆
+     *
+     * @return {@link R}
+     */
     @GetMapping("/logout")
     public R logout() {
         if (!StpUtil.isLogin()) {
--- loast-web/src/main/java/com/loast/controller/UserController.java
+++ loast-web/src/main/java/com/loast/controller/UserController.java
@@ -12,7 +12,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * 用户(User)表控制层
+ * 用户接口
  *
  * @author Arman
  * @since 2021-11-03 21:14:12
@@ -25,6 +25,12 @@ public class UserController {
     @Resource
     private IUserService userService;
 
+    /**
+     * 用户注册
+     *
+     * @param userModel 用户对象
+     * @return {@link R}
+     */
     @PostMapping("/register")
     public R registerUser(@RequestBody @Validated UserModel userModel, HttpServletRequest request) {
         log.info("用户注册接口：userModel:{}", userModel);
@@ -32,12 +38,17 @@ public class UserController {
         return R.ok("注册成功，请注意邮箱的激活地址~");
     }
 
+    /**
+     * 激活账号
+     *
+     * @param code 激活码
+     * @return {@link R}
+     */
     @GetMapping("active")
     public R active(@RequestParam("code") String code) {
         userService.activeUser(code);
         return R.ok();
     }
-
 
 
 }
